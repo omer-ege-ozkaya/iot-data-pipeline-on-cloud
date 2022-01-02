2d4b22be7d6fb78861a2572c46dd6900d8776b33
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-01-02 17:49:21 +0300

    Init bugfix.

--- iot-data-generator/init.sh
+++ iot-data-generator/init.sh
@@ -1,12 +1,13 @@
 export PROJECT_ID=$(gcloud config list --format "value(core.project)")
 export MY_REGION=us-central1
+export MY_REGISTRY=swe590-iot-registry
 source venv/bin/activate
 
 function start-iot-json-generator() {
   python cloudiot_mqtt_example_json.py \
      --project_id="$PROJECT_ID" \
      --cloud_region=$MY_REGION \
-     --registry_id=iotlab-registry \
+     --registry_id=$MY_REGISTRY \
      --device_id=swe590-sensor-"$1" \
      --private_key_file=rsa_private.pem \
      --message_type=event \
@@ -17,7 +18,7 @@ function start-iot-generator() {
   python cloudiot_mqtt_example.py \
      --project_id="$PROJECT_ID" \
      --cloud_region=$MY_REGION \
-     --registry_id=iotlab-registry \
+     --registry_id=$MY_REGISTRY \
      --device_id=swe590-sensor-"$1" \
      --private_key_file=rsa_private.pem \
      --message_type=event \

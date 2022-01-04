27d905d86b622b608fda519fc0e9a3a2265e9a33
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-01-04 20:35:09 +0300

    Cleanup of files not used.

--- iot-data-generator/init.sh
+++ iot-data-generator/init.sh
@@ -16,16 +16,5 @@ function start-iot-json-generator() {
      --num_messages=1000
 }
 
-function start-iot-generator() {
-  python cloudiot_mqtt_example.py \
-     --project_id="$PROJECT_ID" \
-     --cloud_region=$MY_REGION \
-     --registry_id=$MY_REGISTRY \
-     --device_id=swe590-sensor-"$1" \
-     --private_key_file=rsa_private.pem \
-     --message_type=event \
-     --algorithm=RS256
-}
-
-printf '\nUse "start-iot-json-generator" or "start-iot-generator" commands with argument "1" or "2" as the sensor id.'
+printf '\nUse "start-iot-json-generator" command with argument "1" or "2" as the sensor id.'
 printf '\nExample: "start-iot-json-generator 1"\n\n'

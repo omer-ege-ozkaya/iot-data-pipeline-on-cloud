31f8f8b1320f0d80cd2fcd62c3d70ef50e0cc17f
Omer Ege Ozkaya <ege.ozkaya@gmail.com>
2022-01-06 03:48:12 +0300

    First join commit.

--- data-transformation/java/swe590-dataflow-project/src/main/java/com/omeregeozkaya/boun/StarterPipeline.java
+++ data-transformation/java/swe590-dataflow-project/src/main/java/com/omeregeozkaya/boun/StarterPipeline.java
@@ -26,16 +26,21 @@ import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.schemas.transforms.Join;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.display.DisplayData;
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
+import org.apache.beam.sdk.transforms.join.CoGroupByKey;
+import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TupleTag;
 import org.apache.beam.sdk.values.TypeDescriptor;
 import org.joda.time.Duration;
 import org.slf4j.Logger;
@@ -112,20 +117,26 @@ public class StarterPipeline {
 //            .apply("Write files to GCS", new WriteOneFilePerWindow(options.getOutput(), numShards));
 
 //        PCollection<String> messages = pipeline.apply("Read PubSub Messages", PubsubIO.readStrings().fromTopic(options.getInputTopic()));
-        List<String> testData = Arrays.asList(
-            "{\"timestamp\": \"2021-06-12\", \"device\": \"istanbul\", \"temperature\": \"22\"}",
-            "{\"timestamp\": \"2021-07-13\", \"device\": \"istanbul\", \"temperature\": \"18\"}",
-            "{\"timestamp\": \"2022-08-14\", \"device\": \"ordu\", \"temperature\": \"24\"}",
-            "{\"timestamp\": \"2022-09-15\", \"device\": \"ordu\", \"temperature\": \"19\"}"
+        final List<String> testData = Arrays.asList(
+            "{\"timestamp\": \"2021-06-12\", \"device\": \"ist\", \"temperature\": \"22\"}",
+            "{\"timestamp\": \"2021-07-13\", \"device\": \"ist\", \"temperature\": \"18\"}",
+            "{\"timestamp\": \"2022-08-14\", \"device\": \"ord\", \"temperature\": \"24\"}",
+            "{\"timestamp\": \"2022-09-15\", \"device\": \"ord\", \"temperature\": \"19\"}"
         );
         PCollection<String> rawMessages = pipeline.apply("Creation of data", Create.of(testData));
+        final List<KV<String, String>> testLookUpData = Arrays.asList(
+            KV.of("ist", "İstanbul"),
+            KV.of("ord", "Ordu"),
+            KV.of("izm", "İzmir")
+        );
+        PCollection<KV<String, String>> lookUpData = pipeline.apply("Creation of lookup data", Create.of(testLookUpData));
         PCollection<JsonMessage> jsonMessage = rawMessages.apply("Message to JsonMessage", ParDo.of(
                 new DoFn<String, JsonMessage>() {
                     @ProcessElement
                     public void processElement(@Element String message, OutputReceiver<JsonMessage> out) {
                         Gson gson = new Gson();
                         JsonMessage jsonMessage = gson.fromJson(message, JsonMessage.class);
-                        System.out.println(jsonMessage);
+//                        System.out.println(jsonMessage);
                         out.output(jsonMessage);
                     }
                 }
@@ -136,7 +147,7 @@ public class StarterPipeline {
                     @ProcessElement
                     public void processElement(@Element JsonMessage jsonMessage, OutputReceiver<KV<String, JsonMessage>> out) {
                         KV<String, JsonMessage> kv = KV.of(jsonMessage.getDevice(), jsonMessage);
-                        System.out.println(kv);
+//                        System.out.println(kv);
                         out.output(kv);
                     }
                 }
@@ -152,19 +163,110 @@ public class StarterPipeline {
                     public void processElement(@Element KV<String, Iterable<JsonMessage>> kv, OutputReceiver<KV<String, Double>> out) {
                         int sum = 0;
                         int count = 0;
-                        for(JsonMessage jsonMessage : Objects.requireNonNull(kv.getValue())) {
+                        for (JsonMessage jsonMessage : Objects.requireNonNull(kv.getValue())) {
                             sum += Integer.parseInt(jsonMessage.getTemperature());
                             count++;
                         }
                         Double average = (double) sum / count;
                         KV<String, Double> averageOfDevice = KV.of(kv.getKey(), average);
-                        System.out.println(averageOfDevice);
+//                        System.out.println(averageOfDevice);
                         out.output(averageOfDevice);
                     }
                 }
             )
         );
+        final TupleTag<Double> averageTemperatureByDeviceTag = new TupleTag<>();
+        final TupleTag<String> lookUpDataTag = new TupleTag<>();
+        PCollection<KV<String, CoGbkResult>> joined =
+            KeyedPCollectionTuple
+                .of(averageTemperatureByDeviceTag, averageTemperatureByDevice)
+                .and(lookUpDataTag, lookUpData)
+                .apply("Join by cogroupbykey", CoGroupByKey.create());
+        PCollection<String> test = joined.apply("Test", ParDo.of(
+            new DoFn<KV<String, CoGbkResult>, String>() {
+                @ProcessElement
+                public void processElement(ProcessContext processContext) {
+                    KV<String, CoGbkResult> result = processContext.element();
+                    String key = result.getKey();
+                    Iterable<Double> temp = result.getValue().getAll(averageTemperatureByDeviceTag);
+                    Iterable<String> city = result.getValue().getAll(lookUpDataTag);
+                    System.out.println(key + temp.toString() + city.toString());
+                }
+            }
+        ) );
 
+
+
+//        final List<KV<String, String>> emailsList =
+//            Arrays.asList(
+//                KV.of("amy", "amy@example.com"),
+//                KV.of("carl", "carl@example.com"),
+//                KV.of("julia", "julia@example.com"),
+//                KV.of("carl", "carl@email.com"));
+//
+//        final List<KV<String, String>> phonesList =
+//            Arrays.asList(
+//                KV.of("amy", "111-222-3333"),
+//                KV.of("james", "222-333-4444"),
+//                KV.of("amy", "333-444-5555"),
+//                KV.of("carl", "444-555-6666"));
+//
+//        PCollection<KV<String, String>> emails = pipeline.apply("CreateEmails", Create.of(emailsList));
+//        PCollection<KV<String, String>> phones = pipeline.apply("CreatePhones", Create.of(phonesList));
+//        final TupleTag<String> emailsTag = new TupleTag<>();
+//        final TupleTag<String> phonesTag = new TupleTag<>();
+//
+//        final List<KV<String, CoGbkResult>> expectedResults =
+//            Arrays.asList(
+//                KV.of(
+//                    "amy",
+//                    CoGbkResult.of(emailsTag, Arrays.asList("amy@example.com"))
+//                        .and(phonesTag, Arrays.asList("111-222-3333", "333-444-5555"))
+//                ),
+//                KV.of(
+//                    "carl",
+//                    CoGbkResult.of(emailsTag, Arrays.asList("carl@email.com", "carl@example.com"))
+//                        .and(phonesTag, Arrays.asList("444-555-6666"))
+//                ),
+//                KV.of(
+//                    "james",
+//                    CoGbkResult.of(emailsTag, Arrays.asList())
+//                        .and(phonesTag, Arrays.asList("222-333-4444"))
+//                ),
+//                KV.of(
+//                    "julia",
+//                    CoGbkResult.of(emailsTag, Arrays.asList("julia@example.com"))
+//                        .and(phonesTag, Arrays.asList())
+//                )
+//            );
+//
+//        PCollection<KV<String, CoGbkResult>> results =
+//            KeyedPCollectionTuple.of(emailsTag, emails)
+//                .and(phonesTag, phones)
+//                .apply(CoGroupByKey.create());
+//
+//        PCollection<String> contactLines =
+//            results.apply(
+//                ParDo.of(
+//                    new DoFn<KV<String, CoGbkResult>, String>() {
+//                        @ProcessElement
+//                        public void processElement(ProcessContext c) {
+//                            KV<String, CoGbkResult> e = c.element();
+//                            String name = e.getKey();
+//                            Iterable<String> emailsIter = e.getValue().getAll(emailsTag);
+//                            Iterable<String> phonesIter = e.getValue().getAll(phonesTag);
+//                            String formattedResult =
+//                                Snippets.formatCoGbkResults(name, emailsIter, phonesIter);
+//                            c.output(formattedResult);
+//                        }
+//                    }));
+//
+//        final List<String> formattedResults =
+//            Arrays.asList(
+//                "amy; ['amy@example.com']; ['111-222-3333', '333-444-5555']",
+//                "carl; ['carl@email.com', 'carl@example.com']; ['444-555-6666']",
+//                "james; []; ['222-333-4444']",
+//                "julia; ['julia@example.com']; []");
 
         pipeline.run().waitUntilFinish();
     }
